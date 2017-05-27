323c979dc8ccfbadb0c40f393d5da395a5bdebfe
Matheus Frata <msfrata@hotmail.com>
2017-05-27 19:48:18 -0300

    adding the basic structure of main filey

--- server/msp_interface/connect_db_test.py
+++ server/msp_interface/connect_db_test.py
@@ -8,19 +8,14 @@ database = 'rocket_server'
 # Simple routine to run a query on a database and print the results:
 def doQuery( conn ) :
     cur = conn.cursor()
-    list = []
     cur.execute( "SELECT * FROM students" )
 
     for id, name, credits in cur.fetchall() :
         print id, name, credits
 
-
-print 'psycopg2'
-import psycopg2
 try:
     myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database )
     doQuery( myConnection )
-    print 'connected with psycopg2'
 except:
     print "did not work on psyco"
 finally:
