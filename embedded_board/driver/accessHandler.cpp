f977aa81b99403c32522a6119c25223b8896b26e
Unknown <brunoeiterer@gmail.com>
2017-06-19 11:17:11 -0300

    make changes to make code compatible with CCS

--- embedded_board/driver/accessHandler.cpp
+++ embedded_board/driver/accessHandler.cpp
@@ -5,6 +5,10 @@ accessHandler::accessHandler(): P1(P1_address), P4(P4_address){
     P4.setPinDirection(BIT7, output);
 }
 
+accessHandler::~accessHandler(){
+
+}
+
 bool accessHandler::accessRequestHandler(uint8_t request_ID, dataManagement &manager){
 
     if(manager.hasEnoughCredit(request_ID) == false){
@@ -36,7 +40,7 @@ void accessHandler::leaveRequestHandler(uint8_t request_ID, dataManagement &mana
 void accessHandler::openTurnstile(void){
     P1.clearPin(BIT0);
     P4.setPin(BIT7);
-    __delay_cycles(10*4000000);
+    __delay_cycles(10*400000);
     closeTurnstile();
 
 }
--- embedded_board/driver/dataManagement.cpp
+++ embedded_board/driver/dataManagement.cpp
@@ -9,18 +9,26 @@ dataManagement::~dataManagement(){
 }
 
 bool dataManagement::hasEnoughCredit(uint8_t ID){
-    char ID_str[2];
-    sprintf(ID_str, "%d", ID);
-    if(registeredPeopleList.getRegCred(ID_str) > 0)
+    char* ID_str;
+    ID_str = (char*) malloc(2);
+    sprintf(ID_str, "%2.2d", ID);
+    if(registeredPeopleList.getRegCred(ID_str) > 0){
+    	free(ID_str);
         return true;
-    else
+    }
+    else{
+    	free(ID_str);
         return false;
+    }
+
 }
 
 void dataManagement::subtractCredit(uint8_t ID){
-    char ID_str[2];
-    sprintf(ID_str, "%d", ID);
+    char* ID_str;
+    ID_str = (char*) malloc(2);
+    sprintf(ID_str, "%2.2d", ID);
     registeredPeopleList.decreaseRegCred(ID_str);
+    free(ID_str);
 }
 
 void dataManagement::insertInRU(Student* new_student){
