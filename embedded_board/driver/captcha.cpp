64f009d8c7fac6b0dd71e276773b5f59f6171881
Unknown <brunoeiterer@gmail.com>
2017-06-19 21:17:33 -0300

    add verifyCaptcha method

--- embedded_board/driver/captcha.cpp
+++ embedded_board/driver/captcha.cpp
@@ -1,7 +1,11 @@
 #include "captcha.h"
 
-captcha::captcha(){
-
+captcha::captcha(): P3(P3_address){
+	P3.setPinDirection(BIT0, output);
+	P3.setPinDirection(BIT1, output);
+	P3.setPinDirection(BIT2, input);
+	P3.setPinDirection(BIT3, input);
+	P3OUT = 0;
 }
 
 captcha::~captcha(){
@@ -9,5 +13,15 @@ captcha::~captcha(){
 }
 
 uint8_t captcha::getRandomNumber(void){
-	return (uint8_t) ((rand() % 4) + 1);
+	uint8_t aux;
+	aux = (rand() % 3) + 1;
+	P3OUT = aux;
+	return aux;
+}
+
+bool captcha::verifyCaptcha(uint8_t captcha){
+	if (captcha == ((P3IN & 0x0C) >> 2))
+		return true;
+	else return false;
+
 }
--- embedded_board/driver/captcha.h
+++ embedded_board/driver/captcha.h
@@ -3,14 +3,17 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include "io.h"
 
 class captcha{
 private:
+	port P3;
 
 public:
 	captcha();
 	~captcha();
 	uint8_t getRandomNumber(void);
+	bool verifyCaptcha(uint8_t captcha);
 
 };
 
